6b9d6d88c1ab43372f54abd61f0b38c8859c4982
Iván Sánchez <mazyvan@hotmail.com>
2017-05-30 23:17:15 -0500

    working for now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,43 @@ export class AppComponent {
   public x_axis = undefined;
   public y_axis = undefined;
   public color = undefined;
-  constructor(private elRef:ElementRef) { }
+  constructor(private elRef: ElementRef) {
+    setTimeout(() => this.drawCircle(15, 12, 12), 3000);
+  }
 
-  paint() {
+  paintPixel(x: number, y: number) {
+    this.elRef.nativeElement.querySelector('.pixel[data-pos-x="' + x + '"][data-pos-y="' + y + '"]').style.background = 'blue';
+  }
+
+  btnPaintIt() {
     this.elRef.nativeElement.querySelector('.pixel[data-pos-x="' + this.x_axis + '"][data-pos-y="' + this.y_axis + '"]').style.background = this.color;
   }
+
+
+  drawCircle(x0: number, y0: number, radius: number) {
+    console.log('starting drawCircle function');
+    let x: number = radius;
+    let y: number = 0;
+    let err = 0;
+    while (x >= y) {
+      this.paintPixel(x0 + x, y0 + y);
+      this.paintPixel(x0 + y, y0 + x);
+      this.paintPixel(x0 - y, y0 + x);
+      this.paintPixel(x0 - x, y0 + y);
+      this.paintPixel(x0 - x, y0 - y);
+      this.paintPixel(x0 - y, y0 - x);
+      this.paintPixel(x0 + y, y0 - x);
+      this.paintPixel(x0 + x, y0 - y);
+
+      y += 1;
+      if (err <= 0) {
+        err += 2 * y + 1;
+      }
+      if (err > 0) {
+        x -= 1;
+        err -= 2 * x + 1;
+      }
+    }
+  }
+
 }
