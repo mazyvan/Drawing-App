020ef7b51504de2c3d9d9d18af2fa125319d8ed8
Iván Sánchez <mazyvan@hotmail.com>
2017-05-31 01:32:16 -0500

    finished for now!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,33 +10,76 @@ export class AppComponent {
   public x_axis = undefined;
   public y_axis = undefined;
   public color = undefined;
+
+  public circle_x_axis = undefined;
+  public circle_y_axis = undefined;
+  public circle_radius = undefined;
+  public circle_color = undefined;
+
+  public line_x1_axis = undefined;
+  public line_y1_axis = undefined;
+  public line_x2_axis = undefined;
+  public line_y2_axis = undefined;
+  public line_color = undefined;
+
+  public square_x1_axis = undefined;
+  public square_y1_axis = undefined;
+  public square_x2_axis = undefined;
+  public square_y2_axis = undefined;
+  public square_color = undefined;
+
   constructor(private elRef: ElementRef) {
-    setTimeout(() => this.drawCircle(15, 12, 12), 3000);
+    // setTimeout(() => this.drawingFuncitionsTest(), 4000);
   }
 
+  // BASIC FUNCTIONS - BASIC FUNCTIONS - BASIC FUNCTIONS - BASIC FUNCTIONS - BASIC FUNCTIONS
   paintPixel(x: number, y: number, color: string) {
-    this.elRef.nativeElement.querySelector('.pixel[data-pos-x="' + x + '"][data-pos-y="' + y + '"]').style.background = color;
+    try {
+      this.elRef.nativeElement.querySelector('.pixel[data-pos-x="' + x + '"][data-pos-y="' + y + '"]').style.background = color;
+    } catch (error) {
+      console.log(error);
+    }
   }
 
-  btnPaintIt() {
+  cleanPixel(x: number, y: number) {
+    this.paintPixel(x, y, 'white');
+  }
+
+  // BUTTONS - BUTTONS - BUTTONS - BUTTONS - BUTTONS - BUTTONS - BUTTONS - BUTTONS - BUTTONS 
+  btnPaintPixel() {
     this.paintPixel(this.x_axis, this.y_axis, this.color);
   }
 
+  btnCleanPixel() {
+    this.cleanPixel(this.x_axis, this.y_axis);
+  }
 
-  drawCircle(x0: number, y0: number, radius: number) {
-    console.log('starting drawCircle function');
+  btnDrawLineShape() {
+    this.drawLineShape(this.line_x1_axis, this.line_y1_axis, this.line_x2_axis, this.line_y2_axis, this.line_color);
+  }
+
+  btnDrawSquareShape() {
+    this.drawSquareShape(this.square_x1_axis, this.square_y1_axis, this.square_x2_axis, this.square_y2_axis, this.square_color);
+  }
+
+  btnDrawCircleShape() {
+    this.drawCircleShape(this.circle_x_axis, this.circle_y_axis, this.circle_radius, this.circle_color);
+  }
+
+  // SHAPES - SHAPES - SHAPES - SHAPES - SHAPES - SHAPES - SHAPES - SHAPES - SHAPES - SHAPES 
+  drawCircleShape(x1: number, y1: number, radius: number, color: string) {
     let x: number = radius;
     let y: number = 0;
     let err = 0;
     while (x >= y) {
-      this.paintPixel(x0 + x, y0 + y, 'blue');
-      this.paintPixel(x0 + y, y0 + x, 'blue');
-      this.paintPixel(x0 - y, y0 + x, 'blue');
-      this.paintPixel(x0 - x, y0 + y, 'blue');
-      this.paintPixel(x0 - x, y0 - y, 'blue');
-      this.paintPixel(x0 - y, y0 - x, 'blue');
-      this.paintPixel(x0 + y, y0 - x, 'blue');
-      this.paintPixel(x0 + x, y0 - y, 'blue');
+      this.paintPixel(x1 + x, y1 + y, color);
+      this.paintPixel(x1 + y, y1 + x, color);
+      this.paintPixel(x1 - y, y1 + x, color);
+      this.paintPixel(x1 - x, y1 + y, color);
+      this.paintPixel(x1 - x, y1 - y, color);
+      this.paintPixel(x1 - y, y1 - x, color);
+      this.paintPixel(x1 + y, y1 - x, color);
+      this.paintPixel(x1 + x, y1 - y, color);
 
       y += 1;
       if (err <= 0) {
@@ -49,4 +92,50 @@ export class AppComponent {
     }
   }
 
+  drawLineShape(x1: number, y1: number, x2: number, y2: number, color: string) {
+    if (y1 == y2 && x1 < x2) {
+      do {
+        this.paintPixel(x1, y1, color);
+        x1++;
+      } while (x1 <= x2)
+    }
+    else if (y1 == y2 && x1 > x2) {
+      do {
+        this.paintPixel(x1, y1, color);
+        x1--;
+      } while (x1 >= x2)
+    }
+    else if (x1 == x2 && y1 < y2) {
+      do {
+        this.paintPixel(x1, y1, color);
+        y1++;
+      } while (y1 <= y2)
+    }
+    else if (x1 == x2 && y1 > y2) {
+      do {
+        this.paintPixel(x1, y1, color);
+        y1--;
+      } while (y1 >= y2)
+    }
+  }
+
+  drawSquareShape(x1: number, y1: number, x2: number, y2: number, color: string) {
+    this.drawLineShape(x1, y1, x2, y1, color);
+    this.drawLineShape(x2, y1, x2, y2, color);
+    this.drawLineShape(x2, y2, x1, y2, color);
+    this.drawLineShape(x1, y2, x1, y1, color);
+  }
+
+
+  // UNIT TESTS - UNIT TESTS - UNIT TESTS - UNIT TESTS - UNIT TESTS - UNIT TESTS - UNIT TESTS
+  drawingFuncitionsTest() {
+    this.drawLineShape(2, 3, 2, 20, 'red'); // Draw vertical line (top-bottom)
+    this.drawLineShape(5, 15, 5, 5, 'green'); // Draw vertical line (bottom-top)
+    this.drawLineShape(2, 3, 15, 3, 'blue'); // Draw horizontal line (left-right)
+    this.drawLineShape(13, 7, 0, 7, 'yellow'); // Draw horizontal line (right-left)
+
+    this.drawCircleShape(17, 9, 6, 'purple'); // Draw circle
+
+    this.drawSquareShape(9, 16, 38, 23, 'black'); // Draw square
+  }
 }
